51c5a2967f8f4e643e7aaf0e22ea0de3387b480c
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-11-21 11:26:03 +0800

    Update backup.sh

--- system/backup.sh
+++ system/backup.sh
@@ -14,23 +14,16 @@ NC='\e[0m'
 clear
 IP=$(wget -qO- icanhazip.com);
 date=$(date +"%Y-%m-%d")
+domain=$(cat /usr/local/etc/xray/domain)
 clear
 echo " VPS Data Backup By NevermoreSSH "
 sleep 1
-echo ""
-echo -e "[ ${green}INFO${NC} ] Please Insert Password To Secure Backup Data ."
-echo ""
-read -rp "Enter password : " -e InputPass
-clear
-sleep 1
-if [[ -z $InputPass ]]; then
-exit 0
-fi
 echo -e "[ ${green}INFO${NC} ] Processing . . . "
 mkdir -p /root/backup
 sleep 1
 clear
 echo " Please Wait VPS Data Backup In Progress . . . "
+echo " Backup SSH & XRAY Account . . . "
 #cp -r /root/.acme.sh /root/backup/ &> /dev/null
 #cp -r /var/lib/premium-script/ /root/backup/premium-script
 #cp -r /usr/local/etc/xray /root/backup/xray
@@ -43,9 +36,9 @@ cp -r /etc/gshadow /root/backup/gshadow >/dev/null 2>&1
 cp -r /etc/passwd /root/backup/passwd >/dev/null 2>&1
 cp -r /etc/group /root/backup/group >/dev/null 2>&1
 cd /root
-zip -rP $InputPass $IP-$date.zip backup > /dev/null 2>&1
-rclone copy /root/$IP-$date.zip dr:backup/
-url=$(rclone link dr:backup/$IP-$date.zip)
+zip -r $IP-$date_$domain.zip backup > /dev/null 2>&1
+rclone copy /root/$IP-$date_$domain.zip dr:backup/
+url=$(rclone link dr:backup/$IP-$date_$domain.zip)
 id=(`echo $url | grep '^https' | cut -d'=' -f2`)
 link="https://drive.google.com/u/4/uc?id=${id}&export=download"
 clear
@@ -56,11 +49,9 @@ echo "Please Copy Link Below & Save In Notepad"
 echo ""
 echo -e "Your VPS IP ( \033[1;37m$IP\033[0m )"
 echo ""
-echo -e "Your VPS Data Backup Password : \033[1;37m$InputPass\033[0m"
-echo ""
 echo -e "\033[1;37m$link\033[0m"
 echo ""
 echo "If you want to restore data, please enter the link above"
 rm -rf /root/backup
-rm -r /root/$IP-$date.zip
+rm -r /root/$IP-$date_$domain.zip
 echo ""
