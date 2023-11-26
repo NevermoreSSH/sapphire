a42df65d2501ad703fc8d0d28a8f4175df77aa30
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-11-26 19:23:05 +0800

    Update xraay.sh

--- menu/xraay.sh
+++ menu/xraay.sh
@@ -1,33 +1,6 @@
 #wget https://github.com/${GitUser}/
 GitUser="NevermoreSSH"
 
-# // IZIN SCRIPT
-export MYIP=$(curl -sS ipv4.icanhazip.com)
-
-# Valid Script
-VALIDITY () {
-    clear
-    today=`date -d "0 days" +"%Y-%m-%d"`
-    Exp1=$(curl -sS https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | grep $MYIP | awk '{print $4}')
-    if [[ $today < $Exp1 ]]; then
-    echo -e "\e[32mYOUR SCRIPT ACTIVE..\e[0m"
-    else
-    echo -e "\e[31mYOUR SCRIPT HAS EXPIRED!\e[0m";
-    echo -e "\e[31mPlease renew your ipvps first\e[0m"
-    exit 0
-fi
-}
-IZIN=$(curl -sS https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
-if [ $MYIP = $IZIN ]; then
-echo -e "\e[32mPermission Accepted...\e[0m"
-VALIDITY
-sleep 0.1
-else
-echo -e "\e[31mPermission Denied!\e[0m";
-echo -e "\e[31mPlease buy script first\e[0m"
-exit 0
-fi
-
 # // PROVIDED
 clear
 source /var/lib/premium-script/ipvps.conf
