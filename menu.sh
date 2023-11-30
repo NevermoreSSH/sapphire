f8bb6117e77fd5b364f4cb058730fa835269adeb
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-11-30 14:50:00 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,220 +1,187 @@
 #!/bin/bash
-#wget https://github.com/${GitUser}/
-GitUser="NevermoreSSH"
-#IZIN SCRIPT
-MYIP=$(curl -sS ipv4.icanhazip.com)
 clear
-#Domain
-domain=$(cat /usr/local/etc/xray/domain)
-ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10)
-CITY=$(curl -s ipinfo.io/city)
-WKT=$(curl -s ipinfo.io/timezone)
+m="\033[0;1;36m"
+y="\033[0;1;37m"
+yy="\033[0;1;32m"
+yl="\033[0;1;33m"
+wh="\033[0m"
+## Foreground
+DEFBOLD='\e[39;1m'
+RB='\e[31;1m'
+GB='\e[32;1m'
+YB='\e[33;1m'
+BB='\e[34;1m'
+MB='\e[35;1m'
+CB='\e[35;1m'
+WB='\e[37;1m'
+#Domain & IPVPS
+domain=$(cat /root/domain)
 IPVPS=$(curl -s ipinfo.io/ip)
-cname=$(awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo)
-cores=$(awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo)
-freq=$(awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo)
-tram=$(free -m | awk 'NR==2 {print $2}')
-uram=$(free -m | awk 'NR==2 {print $3}')
-fram=$(free -m | awk 'NR==2 {print $4}')
-clear
 # OS Uptime
 uptime="$(uptime -p | cut -d " " -f 2-10)"
-# USERNAME
-rm -f /usr/bin/user
-username=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | grep $MYIP | awk '{print $2}')
-echo "$username" >/usr/bin/user
-# Order ID
-rm -f /usr/bin/ver
-user=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | grep $MYIP | awk '{print $3}')
-echo "$user" >/usr/bin/ver
-# validity
-rm -f /usr/bin/e
-valid=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | grep $MYIP | awk '{print $4}')
-echo "$valid" >/usr/bin/e
-# DETAIL ORDER
-username=$(cat /usr/bin/user)
-oid=$(cat /usr/bin/ver)
-exp=$(cat /usr/bin/e)
-clear
-version=$(cat /home/ver)
-ver=$( curl https://raw.githubusercontent.com/${GitUser}/version/main/version.conf )
-clear
-# CEK UPDATE
-Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
-Info1="${Green_font_prefix}($version)${Font_color_suffix}"
-Info2="${Green_font_prefix}(LATEST VERSION)${Font_color_suffix}"
-Error="Version ${Green_font_prefix}[$ver]${Font_color_suffix} available${Red_font_prefix}[Please Update]${Font_color_suffix}"
-version=$(cat /home/ver)
-new_version=$( curl https://raw.githubusercontent.com/${GitUser}/version/main/version.conf | grep $version )
-#Status Version
-if [ $version = $new_version ]; then
-stl="${Info2}"
-else
-stl="${Error}"
-fi
-clear
-# Getting CPU Information
-cpu_usage1="$(ps aux | awk 'BEGIN {sum=0} {sum+=$3}; END {print sum}')"
-cpu_usage="$((${cpu_usage1/\.*/} / ${corediilik:-1}))"
-cpu_usage+=" %"
-#Download/Upload today
-dtoday="$(vnstat -i eth0 | grep "today" | awk '{print $2" "substr ($3, 1, 1)}')"
-utoday="$(vnstat -i eth0 | grep "today" | awk '{print $5" "substr ($6, 1, 1)}')"
+# RAM Info
+tram=$(free -m | awk 'NR==2 {print $2}')
+uram=$(free -m | awk 'NR==2 {print $3}')
+# TOTAL ACC XRAYS WS & XTLS
+vmess=$(grep -c -E "^### $user" "/usr/local/etc/xray/config.json")
+vless=$(grep -c -E "^### $user" "/usr/local/etc/xray/vless.json")
+trws=$(grep -c -E "^### $user" "/usr/local/etc/xray/trojanws.json")
+txtls=$(grep -c -E "^### $user" "/usr/local/etc/xray/xtrojan.json")
+tr=$(grep -c -E "^### $user" "/usr/local/etc/xray/trojan.json")
+# Total BANDWIDTH
 ttoday="$(vnstat -i eth0 | grep "today" | awk '{print $8" "substr ($9, 1, 1)}')"
-#Download/Upload yesterday
-dyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $2" "substr ($3, 1, 1)}')"
-uyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $5" "substr ($6, 1, 1)}')"
-tyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $8" "substr ($9, 1, 1)}')"
-#Download/Upload current month
-dmon="$(vnstat -i eth0 -m | grep "$(date +"%b '%y")" | awk '{print $3" "substr ($4, 1, 1)}')"
-umon="$(vnstat -i eth0 -m | grep "$(date +"%b '%y")" | awk '{print $6" "substr ($7, 1, 1)}')"
 tmon="$(vnstat -i eth0 -m | grep "$(date +"%b '%y")" | awk '{print $9" "substr ($10, 1, 1)}')"
-
-# STATUS EXPIRED ACTIVE
-Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[4$below" && Font_color_suffix="\033[0m"
-Info="${Green_font_prefix}(Active)${Font_color_suffix}"
-Error="${Green_font_prefix}${Font_color_suffix}${Red_font_prefix}[EXPIRED]${Font_color_suffix}"
-
-today=$(date -d "0 days" +"%Y-%m-%d")
-Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | grep $MYIP | awk '{print $4}')
-if [[ $today < $Exp1 ]]; then
-    sts="${Info}"
-else
-    sts="${Error}"
-fi
-echo -e "\e[32mloading...\e[0m"
 clear
-# CERTIFICATE STATUS
-d1=$(date -d "$valid" +%s)
-d2=$(date -d "$today" +%s)
-certifacate=$(((d1 - d2) / 86400))
-# TOTAL ACC CREATE VMESS WS
-vmess=$(grep -c -E "^#vms " "/usr/local/etc/xray/vmess.json")
-# TOTAL ACC CREATE  VLESS WS
-vless=$(grep -c -E "^#vls " "/usr/local/etc/xray/vless.json")
-# TOTAL ACC CREATE  VLESS TCP XTLS
-xtls=$(grep -c -E "^#vxtls " "/usr/local/etc/xray/config.json")
-# TOTAL ACC CREATE  TROJAN
-trtls=$(grep -c -E "^#trx " "/usr/local/etc/xray/tcp.json")
-# TOTAL ACC CREATE  TROJAN WS TLS
-trws=$(grep -c -E "^#trws " "/usr/local/etc/xray/trojan.json")
-# TOTAL ACC CREATE OVPN SSH
-total_ssh="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
-# PROVIDED
-creditt=$(cat /root/provided)
-# BANNER COLOUR
-banner_colour=$(cat /etc/banner)
-# TEXT ON BOX COLOUR
-box=$(cat /etc/box)
-# LINE COLOUR
-line=$(cat /etc/line)
-# TEXT COLOUR ON TOP
-text=$(cat /etc/text)
-# TEXT COLOUR BELOW
-below=$(cat /etc/below)
-# BACKGROUND TEXT COLOUR
-back_text=$(cat /etc/back)
-# NUMBER COLOUR
-number=$(cat /etc/number)
-# BANNER
-banner=$(cat /usr/bin/bannerku)
-ascii=$(cat /usr/bin/test)
-clear
-echo -e "\e[$banner_colour"
-figlet -f $ascii "$banner"
-echo -e "\e[$text VPS Script"
-echo -e " \e[$line╒════════════════════════════════════════════════════════════╕\e[m"
-echo -e "  \e[$back_text                    \e[30m[\e[$box SERVER INFORMATION\e[30m ]\e[1m                  \e[m"
-echo -e " \e[$line╘════════════════════════════════════════════════════════════╛\e[m"
-echo -e "  \e[$text Cpu Model            :$cname"
-echo -e "  \e[$text Cpu Frequency        :$freq MHz"
-echo -e "  \e[$text Number Of Core       : $cores"
-echo -e "  \e[$text CPU Usage            : $cpu_usage"
-echo -e "  \e[$text Operating System     : "$(hostnamectl | grep "Operating System" | cut -d ' ' -f5-)
-echo -e "  \e[$text Kernel               : $(uname -r)"
-echo -e "  \e[$text RAM Info             : $uram MB / $tram MB"
-echo -e "  \e[$text System Uptime        : $uptime"
-echo -e "  \e[$text Ip Vps/Address       : $IPVPS"
-echo -e "  \e[$text Domain Name          : $domain\e[0m"
-#echo -e "  \e[$text Organization         : $( curl -s ipinfo.io/org )"
-echo -e "  \e[$text Order ID             : $oid"
-echo -e "  \e[$text Expired Status       : $exp $sts"
-echo -e "  \e[$text Provided By          : $creditt"
-echo -e " \e[$line╒════════════════════════════════════════════════════════════╕\e[m"
-echo -e "  \e[$text Traffic\e[0m       \e[${text}Today      Yesterday        Month   "
-echo -e "  \e[$text Download\e[0m   \e[${text}   $dtoday    $dyest       $dmon   \e[0m"
-echo -e "  \e[$text Upload\e[0m     \e[${text}   $utoday    $uyest       $umon   \e[0m"
-echo -e "  \e[$text Total\e[0m       \e[${text}  $ttoday    $tyest       $tmon  \e[0m "
-echo -e " \e[$line╘════════════════════════════════════════════════════════════╛\e[m"
-echo -e " \e[$text Ssh/Ovpn   V2ray   Vless   Vlessxtls   Trojan-Ws   Trojan-Tls \e[0m "    
-echo -e " \e[$below    $total_ssh         $vmess       $vless        $xtls           $trws           $trtls \e[0m "
-echo -e " \e[$line╒════════════════════════════════════════════════════════════╕\e[m"
-echo -e "  \e[$back_text                        \e[30m[\e[$box PANEL MENU\e[30m ]\e[1m                       \e[m"
-echo -e " \e[$line╘════════════════════════════════════════════════════════════╛\e[m"
-echo -e "  \e[$number (•1)\e[m \e[$below XRAY VMESS & VLESS\e[m"
-echo -e "  \e[$number (•2)\e[m \e[$below TROJAN XRAY & WS\e[m"
-echo -e "  \e[$number (•3)\e[m \e[$below SSHWS & OPENVPN\e[m"     
-echo -e " \e[$line╒════════════════════════════════════════════════════════════╕\e[m"
-echo -e "  \e[$back_text                        \e[30m[\e[$box VPS MENU\e[30m ]\e[1m                       \e[m"
-echo -e " \e[$line╘════════════════════════════════════════════════════════════╛\e[m"
-echo -e "  \e[$number (•4)\e[m \e[$below SYSTEM MENU\e[m          \e[$number (•8)\e[m \e[$below MENU THEMES\e[m"
-echo -e "  \e[$number (•5)\e[m \e[$below CHECK RUNNING\e[m        \e[$number (•9)\e[m \e[$below INFO ALL PORT\e[m"
-echo -e "  \e[$number (•6)\e[m \e[$below CHANGE PORT\e[m          \e[$number (10)\e[m \e[$below CLEAR EXPIRED FILES\e[m"
-echo -e "  \e[$number (•7)\e[m \e[$below REBOOT VPS\e[m           \e[$number (11)\e[m \e[$below CLEAR LOG VPS\e[m"
+echo ""
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "      ${WB}Multiport Websocket Autoscript By NevermoreSSH${NC}"
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "               ${WB}»»» Server Information «««${NC}             "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "  ${RB}♦️${NC} ${YB}OS      :  "$(hostnamectl | grep "Operating System" | cut -d ' ' -f5-) ${NC}         
+echo -e "  ${RB}♦️${NC} ${YB}KERNEL  :  $(uname -r) ${NC} "
+echo -e "  ${RB}♦️${NC} ${YB}UPTIME  :  $uptime ${NC} "
+echo -e "  ${RB}♦️${NC} ${YB}RAM     :  $uram MB / $tram MB ${NC} "
+echo -e "  ${RB}♦️${NC} ${YB}DOMAIN  :  $domain ${NC} "
+echo -e "  ${RB}♦️${NC} ${YB}IPVPS   :  $IPVPS ${NC} "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "                   ${WB}»»» XRAYS Menu «««${NC}          "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "  ${RB}01.${NC} ${YB}XRAY VMESS WS    ${WB}[${GB}${vmess}${WB}]${NC}"
+echo -e "  ${RB}02.${NC} ${YB}XRAY VLESS WS    ${WB}[${GB}${vless}${WB}]${NC}"
+echo -e "  ${RB}03.${NC} ${YB}XRAY TROJAN WS   ${WB}[${GB}${trws}${WB}]${NC}"
+echo -e "  ${RB}04.${NC} ${YB}XRAY TROJAN XTLS ${WB}[${GB}${txtls}${WB}]${NC}"
+echo -e "  ${RB}05.${NC} ${YB}XRAY TROJAN TCP  ${WB}[${GB}${tr}${WB}]${NC}"
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "                   ${WB}»»» VPS Menu «««${NC}                 "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "  ${RB}06.${NC} ${YB}INSTALL ADS BLOCK ${NC}"
+echo -e "  ${RB}07.${NC} ${YB}INSTALL TCP BBRPLUS ${NC}"
+echo -e "  ${RB}08.${NC} ${YB}ADS BLOCK MENU ${NC}"
+echo -e "  ${RB}09.${NC} ${YB}DNS CHANGER ${NC}"
+echo -e "  ${RB}10.${NC} ${YB}NETFLIX CHECKER ${NC}"
+echo -e "  ${RB}11.${NC} ${YB}LIMIT BANDWITH SPEED ${NC}"
 echo -e ""
-echo -e "  \e[$below[Ctrl + C] For exit from main menu\e[m"
-echo -e " \e[$line╒════════════════════════════════════════════════════════════╕\e[m"
-echo -e "  \e[$below Version Name         : SSH XRAY WEBSOCKET MULTIPORT v2.0"
-echo -e "  \e[$below Autoscript By        : NEVERMORESSH"
-echo -e "  \e[$below Certificate Status   : Expired in $certifacate days"
-echo -e "  \e[$below Client Name          : $username"
-echo -e " \e[$line╘════════════════════════════════════════════════════════════╛\e[m"
-echo -e "\e[$below "
-read -p " Select menu :  " menu
-echo -e ""
+echo -e "  ${RB}12.${NC} ${YB}CHANGE DOMAIN ${NC}"
+echo -e "  ${RB}13.${NC} ${YB}RENEW CERT XRAYS ${NC}"
+echo -e "  ${RB}14.${NC} ${YB}CHECK VPN STATUS ${NC}"
+echo -e "  ${RB}15.${NC} ${YB}CHECK VPN PORT ${NC}"
+echo -e "  ${RB}16.${NC} ${YB}RESTART VPN SERVICE ${NC}"
+echo -e "  ${RB}17.${NC} ${YB}SPEEDTEST ${NC}"
+echo -e "  ${RB}18.${NC} ${YB}CHECK CPU & RAM ${NC}"
+echo -e "  ${RB}19.${NC} ${YB}CHECK BANDWIDTH ${NC}"
+echo -e "  ${RB}20.${NC} ${YB}BACKUP ${NC}"
+echo -e "  ${RB}21.${NC} ${YB}RESTORE ${NC}"
+echo -e "  ${RB}22.${NC} ${YB}REBOOT ${NC}"
+echo -e "  ${RB}23.${NC} ${YB}XRAY-CORE CHANGER ${NC}"
+#echo -e "  ${RB}24.${NC} ${YB}LOGOUT ${NC}"
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "                 ${WB}»»» Total Bandwidth «««${NC}             "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "  ${RB}♦️${NC} ${YB}Daily Data Usage    : $ttoday ${NC}"
+echo -e "  ${RB}♦️${NC} ${YB}Monthly Data Usage  : $tmon ${NC}"
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo ""
+read -p "Select From Options [ 1 - 23 ] : " menu
 case $menu in
 1)
-    xraay
-    ;;
+clear
+menu-ws
+;;
 2)
-    trojaan
-    ;;
+clear
+menu-vless
+;;
 3)
-    ssh
-    ;;
+clear
+menu-tr
+;;
 4)
-    system
-    ;;
+clear
+menu-xrt
+;;
 5)
-    check-sc
-    ;;
+clear
+menu-xtr
+;;
 6)
-    change-port
-    ;;
-7)
-    reboot
-    ;;
+clear
+ins-helium
+;;
 8)
-    themes
-    ;;
+clear
+helium
+;;
 9)
-    cat log-install.txt
-    ;;
+clear
+dns
+;;
 10)
-    delete && xp
-    ;;
+clear
+nf
+;;
+7)
+clear
+bbr
+;;
 11)
-    clear-log
-    ;;
-x)
-    clear
-    exit
-    echo -e "\e[1;31mPlease Type menu For More Option, Thank You\e[0m"
-    ;;
+clear
+limit
+;;
+12)
+clear
+add-host
+;;
+13)
+clear
+certxray
+;;
+14)
+clear
+status
+;;
+15)
+clear
+info
+;;
+16)
+clear
+restart
+;;
+17)
+clear
+speedtest
+;;
+18)
+clear
+htop
+;;
+23)
+clear
+wget -q -O /usr/bin/xraychanger "https://raw.githubusercontent.com/NevermoreSSH/Xcore-custompath/main/xraychanger.sh" && chmod +x /usr/bin/xraychanger && xraychanger
+;;
+#24)
+#clear
+#exit && exit && exit
+#;;
+20)
+clear
+backup
+;;
+21)
+clear
+restore
+;;
+22)
+clear
+reboot
+;;
+19)
+clear
+vnstat
+;;
 *)
-    clear
-    echo -e "\e[1;31mPlease enter an correct number\e[0m"
-    sleep 1
-    menu
-    ;;
+clear
+menu
+;;
 esac
