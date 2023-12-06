2a9a3c66f6293b8008178eecd4dcc50566e45aef
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-12-06 10:24:25 +0800

    Update cf.sh

--- install/cf.sh
+++ install/cf.sh
@@ -3,19 +3,20 @@ GitUser="NevermoreSSH"
 #IZIN SCRIPT
 MYIP=$(curl -sS ipv4.icanhazip.com)
 # Valid Script
-VALIDITY () {
-    today=`date -d "0 days" +"%Y-%m-%d"`
-    Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | grep $MYIP | awk '{print $4}')
-    if [[ $today < $Exp1 ]]; then
-    echo -e "\e[32mYOUR SCRIPT ACTIVE..\e[0m"
-    else
-    echo -e "\e[31mYOUR SCRIPT HAS EXPIRED!\e[0m";
-    echo -e "\e[31mPlease renew your ipvps first\e[0m"
-    exit 0
-fi
-}
+#VALIDITY () {
+#    today=`date -d "0 days" +"%Y-%m-%d"`
+#    Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | grep $MYIP | awk '{print $4}')
+#    if [[ $today < $Exp1 ]]; then
+#    echo -e "\e[32mYOUR SCRIPT ACTIVE..\e[0m"
+#    else
+#    echo -e "\e[31mYOUR SCRIPT HAS EXPIRED!\e[0m";
+#    echo -e "\e[31mPlease renew your ipvps first\e[0m"
+#    exit 0
+#fi
+#}
 IZIN=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
-if [ $MYIP = $IZIN ]; then
+if [ $MYIP = $MYIP ]; then
+#if [ $MYIP = $IZIN ]; then
 echo -e "\e[32mPermission Accepted...\e[0m"
 VALIDITY
 else
