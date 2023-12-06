8087f2ebdc2e4e737ee76e42fdd8d1cb4c64ebdd
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-12-06 10:31:57 +0800

    Update ins-xray2.sh

--- install/ins-xray2.sh
+++ install/ins-xray2.sh
@@ -6,20 +6,20 @@ GitUser="NevermoreSSH"
 export MYIP=$(curl -sS ipv4.icanhazip.com)
 
 # // GETTING
-#VALIDITY () {
-#    today=`date -d "0 days" +"%Y-%m-%d"`
-#    Exp1=$(curl -sS https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | grep $MYIP | awk '{print $4}')
-#    if [[ $today < $Exp1 ]]; then
-#    echo -e "\e[32mYOUR SCRIPT ACTIVE..\e[0m"
-#    else
-#    echo -e "\e[31mYOUR SCRIPT HAS EXPIRED!\e[0m";
-#    echo -e "\e[31mPlease renew your ipvps first\e[0m"
-#    exit 0
-#fi
-#}
+VALIDITY () {
+    today=`date -d "0 days" +"%Y-%m-%d"`
+    Exp1=$(curl -sS https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | grep $MYIP | awk '{print $4}')
+    if [[ $today < $Exp1 ]]; then
+    echo -e "\e[32mYOUR SCRIPT ACTIVE..\e[0m"
+    else
+    echo -e "\e[31mYOUR SCRIPT HAS EXPIRED!\e[0m";
+    echo -e "\e[31mPlease renew your ipvps first\e[0m"
+    exit 0
+fi
+}
 IZIN=$(curl -sS https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
-if [ $MYIP = $MYIP ]; then
-#if [ $MYIP = $IZIN ]; then
+#if [ $MYIP = $MYIP ]; then
+if [ $MYIP = $IZIN ]; then
 echo -e "\e[32mPermission Accepted...\e[0m"
 VALIDITY
 else
