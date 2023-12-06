f190da90350d6ca28868108bf3d02d937f9da7a6
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-12-06 10:29:00 +0800

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -47,20 +47,19 @@ echo -e "\e[32mloading...\e[0m"
 clear
 # Valid Script
 #VALIDITY() {
-    #today=$(date -d "0 days" +"%Y-%m-%d")
-    #Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | grep $MYIP | awk '{print $4}')
-    #if [[ $today > $Exp1 ]]; then
-        #echo -e "\e[32mCongratulations! You are Allowed to use AUTOSCRIPT NevermoreSSH..\e[0m"
-        #sleep 5
-    #else
-        #echo -e "\e[31mYOUR SCRIPT HAS EXPIRED!\e[0m"
-        #echo -e "\e[31mPlease renew your ipvps first\e[0m"
-        #exit 0
-    #fi
-#}
+    today=$(date -d "0 days" +"%Y-%m-%d")
+    Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | grep $MYIP | awk '{print $4}')
+    if [[ $today < $Exp1 ]]; then
+        echo -e "\e[32mCongratulations! You are Allowed to use AUTOSCRIPT NevermoreSSH..\e[0m"
+        sleep 5
+    else
+        echo -e "\e[31mYOUR SCRIPT HAS EXPIRED!\e[0m"
+        echo -e "\e[31mPlease renew your ipvps first\e[0m"
+        exit 0
+    fi
+}
 IZIN=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
-if [ $MYIP = $MYIP ]; then
-#if [ $MYIP = $IZIN ]; then
+if [ $MYIP = $IZIN ]; then
     echo -e "\e[32mPermission Accepted...\e[0m"
     VALIDITY
 else
