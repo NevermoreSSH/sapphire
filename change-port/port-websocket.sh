7afafb629deca45b94eeb60f6f9e08fb3693930d
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-12-11 13:09:02 +0800

    Update port-websocket.sh

--- change-port/port-websocket.sh
+++ change-port/port-websocket.sh
@@ -2,6 +2,9 @@
 GitUser="NevermoreSSH"
 #IZIN SCRIPT
 MYIP=$(curl -sS ipv4.icanhazip.com)
+MYIP=$(curl -s ipinfo.io/ip )
+MYIP=$(curl -sS ipv4.icanhazip.com)
+MYIP=$(curl -sS ifconfig.me )
 echo -e "\e[32mloading...\e[0m"
 clear
 # Valid Script
