959f845ff82418cd1063b8ea2301c08f4498a67b
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-12-11 13:12:52 +0800

    Update add-host.sh

--- system/add-host.sh
+++ system/add-host.sh
@@ -10,6 +10,9 @@ GitUser="NevermoreSSH"
 
 # // MY IPVPS
 export MYIP=$(curl -sS ipv4.icanhazip.com)
+MYIP=$(curl -s ipinfo.io/ip )
+MYIP=$(curl -sS ipv4.icanhazip.com)
+MYIP=$(curl -sS ifconfig.me )
 
 # // VALID SCRIPT
 clear
