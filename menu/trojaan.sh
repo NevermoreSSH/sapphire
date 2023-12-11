2c1bc546a5e16407cc509d7a1258e1cf8e4cc5ea
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-12-11 12:49:51 +0800

    Update trojaan.sh

--- menu/trojaan.sh
+++ menu/trojaan.sh
@@ -3,6 +3,9 @@ GitUser="NevermoreSSH"
 
 # // IZIN SCRIPT
 export MYIP=$(curl -sS ipv4.icanhazip.com)
+MYIP=$(curl -s ipinfo.io/ip )
+MYIP=$(curl -sS ipv4.icanhazip.com)
+MYIP=$(curl -sS ifconfig.me )
 
 # // Valid Script
 VALIDITY () {
