259b1b6f158e177f9a67938ec71377f41c6fd0d7
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-12-11 13:17:32 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -3,6 +3,9 @@
 GitUser="NevermoreSSH"
 #IZIN SCRIPT
 MYIP=$(curl -sS ipv4.icanhazip.com)
+MYIP=$(curl -s ipinfo.io/ip )
+MYIP=$(curl -sS ipv4.icanhazip.com)
+MYIP=$(curl -sS ifconfig.me )
 clear
 #Domain
 domain=$(cat /usr/local/etc/xray/domain)
