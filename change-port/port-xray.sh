c4ab739718bbc7e14a0a9631fca1c0957b19a615
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-12-11 13:09:11 +0800

    Update port-xray.sh

--- change-port/port-xray.sh
+++ change-port/port-xray.sh
@@ -3,6 +3,9 @@ GitUser="NevermoreSSH"
 
 # // IZIN SCRIPT
 MYIP=$(curl -sS ipv4.icanhazip.com)
+MYIP=$(curl -s ipinfo.io/ip )
+MYIP=$(curl -sS ipv4.icanhazip.com)
+MYIP=$(curl -sS ifconfig.me )
 export RED='\033[0;31m';
 export NC='\033[0m';
 
