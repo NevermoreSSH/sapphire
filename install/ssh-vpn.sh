03149a5597603c7c82e0225a0210cc2ba03d327a
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2024-01-10 23:50:08 +0800

    Update ssh-vpn.sh

--- install/ssh-vpn.sh
+++ install/ssh-vpn.sh
@@ -372,9 +372,9 @@ chmod +x ins-helium
 echo "0 1 * * * root delete && xp" >> /etc/crontab
 echo "0 8 * * * root clear-log" >> /etc/crontab
 echo "0 9 * * * root /usr/bin/clear-log" >> /etc/crontab
-echo "0 5 * * * root reboot" >> /etc/crontab
 echo "0 3 * * * root /usr/bin/xp" >> /etc/crontab
 echo "0 4 * * * root /usr/bin/delete" >> /etc/crontab
+echo "0 5 * * * root reboot" >> /etc/crontab
 echo "0 6 * * * root backup" >> /etc/crontab
 echo "0 23 * * * root backup" >> /etc/crontab
 
