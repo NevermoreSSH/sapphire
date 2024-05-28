a47fd8f29af8c578126ee8bf56b9d66a31af3bd2
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2024-05-28 09:02:10 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -74,6 +74,10 @@ tyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $8" "substr ($9, 1, 1)}
 dmon="$(vnstat -i eth0 -m | grep "$(date +"%b '%y")" | awk '{print $3" "substr ($4, 1, 1)}')"
 umon="$(vnstat -i eth0 -m | grep "$(date +"%b '%y")" | awk '{print $6" "substr ($7, 1, 1)}')"
 tmon="$(vnstat -i eth0 -m | grep "$(date +"%b '%y")" | awk '{print $9" "substr ($10, 1, 1)}')"
+#Fixed for ubuntu total usage
+dmon="$(vnstat -i eth0 | grep 'total:' | awk '{print $2, $3}')"
+umon="$(vnstat -i eth0 | grep 'total:' | awk '{print $5, $6}')"
+tmon="$(vnstat -i eth0 | grep 'total:' | awk '{print $8, $9}')"
 
 # STATUS EXPIRED ACTIVE
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[4$below" && Font_color_suffix="\033[0m"
