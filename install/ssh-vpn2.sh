b111c0147ef6f9efea3b1e5d0a6c6f3b7ac1f779
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2024-08-22 21:28:36 +0800

    Update ssh-vpn2.sh

--- install/ssh-vpn2.sh
+++ install/ssh-vpn2.sh
@@ -119,7 +119,7 @@ sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g'
 
 # /etc/ssh/sshd_config
 sed -i '/Port 22/a Port 500' /etc/ssh/sshd_config
-sed -i '/Port 22/a Port 40000' /etc/ssh/sshd_config
+#sed -i '/Port 22/a Port 40000' /etc/ssh/sshd_config
 sed -i '/Port 22/a Port 51443' /etc/ssh/sshd_config
 sed -i '/Port 22/a Port 58080' /etc/ssh/sshd_config
 sed -i '/Port 22/a Port 200' /etc/ssh/sshd_config
